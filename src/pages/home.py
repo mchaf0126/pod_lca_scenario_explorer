d9fb998ac6ffad7beada95ab91512f5ae9708038
Mel Chafart <mchaf@uw.edu>
2024-08-15 08:40:32 -0700

    update to landing page

--- src/pages/home.py
+++ src/pages/home.py
@@ -34,6 +34,27 @@ layout = html.Div(
                         html.Br(),
                         dcc.Markdown(
                             '''
+                            #### Scenario Explorer Goals & Structure
+                            '''
+                        ),
+                        dcc.Markdown(
+                            '''
+                            The POD|LCA WBLCA Scenario Explorer is made from a set of structured 
+                            WBLCA template models with pre-built scenarios. The tool allows users 
+                            to enter project information and explore changes to the model results 
+                            related to specific scenarios and methodological decisions.
+
+                            This version, currently in development, is a POC model that will allow 
+                            us to continue to develop background data infrastructure and model code 
+                            base for the future POD LCA tools. For M3.3 Milestone we are proposing 
+                            to create a fully functional model along with updated documentation and 
+                            datasets for use by our internal tool development & framework team.
+                            ''',
+                            className='fw-light'
+                        ),
+                        html.Br(),
+                        dcc.Markdown(
+                            '''
                             #### About the dashboard
                             '''
                         ),
@@ -50,6 +71,11 @@ layout = html.Div(
                                *  **Construction Scenarios**
                                *  **Replacement Scenarios**
                                *  **End-of-life Scenarios**
+                               
+                            An additional section of the dashboard will facilitate selection or
+                            upload of a base building model, or the generation of a detailed, 
+                            structured Bill of Materials (BoM). This element is still in scoping
+                            and development and not yet ready to be shared.
                             ''',
                             className='fw-light'
                         ),
