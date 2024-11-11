7ad998a7199719881eaab499593e113d794c5aaf
Mel Chafart <141696933+mchaf0126@users.noreply.github.com>
2024-11-11 15:02:49 -0800

    development adjustments to the pages, add dropdown for tm

--- src/pages/template_model.py
+++ src/pages/template_model.py
@@ -9,7 +9,7 @@ from src.components.selection import create_dropdown, create_checklist, \
     create_radio
 from src.components.a4_components import create_special_material_form
 
-register_page(__name__, path='/a4_scenario_builder')
+register_page(__name__, path='/template_model')
 
 layout = html.Div(
     children=[
@@ -23,96 +23,63 @@ layout = html.Div(
     ]
 )
 
-# current_file_path = Path(__file__)
-# main_directory = current_file_path.parents[2]
-# data_directory = main_directory.joinpath('data/tally_commercial_includeBC.csv')
+current_file_path = Path(__file__)
+main_directory = current_file_path.parents[2]
 
-# df = pd.read_csv(data_directory, index_col=False)
+config_path = main_directory.joinpath('src/components/config.yml')
 
-# config_path = main_directory.joinpath('src/components/config.yml')
+config = utils.read_yaml(config_path)
+assert config is not None, 'The config dictionary could not be set'
 
-# config = utils.read_yaml(config_path)
-# assert config is not None, 'The config dictionary could not be set'
+tm_dropdown_yaml = config.get('tm_dropdown')
+assert tm_dropdown_yaml is not None, 'The config for tm dropdown could not be set'
 
-# a4_radioitem_yaml = config.get('a4_scenario_radioitem')
-# assert a4_radioitem_yaml is not None, 'The config for a4 radioitem could not be set'
+tm_dropdown = create_dropdown(
+    label=tm_dropdown_yaml['label'],
+    dropdown_list=tm_dropdown_yaml['dropdown_list'],
+    first_item=tm_dropdown_yaml['first_item'],
+    dropdown_id=tm_dropdown_yaml['dropdown_id']
+)
 
-# scope_dropdown_yaml = config.get('scope_dropdown_a4_scenario')
-# assert scope_dropdown_yaml is not None, 'The config for cat. dropdowns could not be set'
 
-# scenario_checklist_yaml = config.get('a4_scenario_checklist')
-# assert scenario_checklist_yaml is not None, 'The config for scenario checklist could not be set'
+card_child = dbc.CardBody(
+    [
+        dbc.Label('Template Model Selector', class_name='fs-5 fw-bold my-0'),
+        html.Hr(),
+        tm_dropdown,
+    ],
+    class_name='pb-0'
+)
 
-# a4_radioitem = create_radio(
-#     label=a4_radioitem_yaml['label'],
-#     radiolist=a4_radioitem_yaml['radiolist'],
-#     first_item=a4_radioitem_yaml['first_item'],
-#     radio_id=a4_radioitem_yaml['radio_id']
-# )
+tm_controls = dbc.Card(
+    [
+        card_child,
+    ],
+    id='dropdown_card'
+)
 
-# scope_dropdown = create_dropdown(
-#     label=scope_dropdown_yaml['label'],
-#     dropdown_list=scope_dropdown_yaml['dropdown_list'],
-#     first_item=scope_dropdown_yaml['first_item'],
-#     dropdown_id=scope_dropdown_yaml['dropdown_id']
-# )
-
-# impact_dropdown = create_checklist(
-#     label=scenario_checklist_yaml['label'],
-#     checklist=scenario_checklist_yaml['checklist'],
-#     first_item=scenario_checklist_yaml['first_item'],
-#     dropdown_id=scenario_checklist_yaml['checklist_id']
-# )
-
-# card_child = dbc.CardBody(
-#     [
-#         dbc.Label('Transportation Scenarios', class_name='fs-5 fw-bold my-0'),
-#         html.Hr(),
-#         a4_radioitem,
-#     ],
-#     class_name='pb-0'
-# )
-# second_child = dbc.CardBody(
-#     id='second_card_body_a4',
-#     class_name='my_0 py-0',
-# )
-# special_mat_1 = create_special_material_form(
-#     label='Target Material 1',
-#     value=1
-# )
-# special_mat_2 = create_special_material_form(
-#     label='Target Material 2',
-#     value=2
-# )
-
-# controls_cont = dbc.Card(
-#     [
-#         card_child,
-#         second_child
-#     ],
-#     id='a4_card'
-# )
-
-# layout = html.Div(
-#     children=[
-#         dbc.Row(
-#             [
-#                 dbc.Col(
-#                     [
-#                         controls_cont
-#                     ], xs=4, sm=4, md=3, lg=3, xl=3, xxl=3
-#                 ),
-#                 dbc.Col(
-#                     [
-#                         dcc.Graph(id="a4_scenario_bar"),
-#                     ], xs=8, sm=8, md=9, lg=9, xl=9, xxl=9
-#                 ),
-#             ],
-#             justify='center',
-#             className='mb-4'
-#         ),
-#     ],
-# )
+layout = html.Div(
+    children=[
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        tm_controls
+                    ], xs=4, sm=4, md=3, lg=3, xl=3, xxl=3
+                ),
+                dbc.Col(
+                    [
+                        dcc.Markdown('''
+                                     THIS IS STILL IN DEVELOPMENT
+                                     ''')
+                    ], xs=8, sm=8, md=9, lg=9, xl=9, xxl=9
+                ),
+            ],
+            justify='center',
+            className='mb-4'
+        ),
+    ],
+)
 
 
 # @callback(
