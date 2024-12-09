855edb6a988779ac222b4c22cae543401c53d31e
Mel Chafart <141696933+mchaf0126@users.noreply.github.com>
2024-12-08 16:31:18 -0800

    feat: add sidebar to whole app

--- src/app.py
+++ src/app.py
@@ -1,8 +1,9 @@
-from dash import Dash, html, page_container, dcc
+from dash import Dash, page_container, dcc
 import dash_bootstrap_components as dbc
 from dash_bootstrap_templates import load_figure_template
 from src.components.header import create_header
 from src.components.footer import create_footer
+from src.components.sidebar import create_sidebar
 
 dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.min.css"
 app = Dash(
@@ -16,6 +17,7 @@ load_figure_template('pulse')
 
 header = create_header()
 footer = create_footer()
+sidebar = create_sidebar()
 
 app.layout = dbc.Container(
     [
@@ -24,32 +26,54 @@ app.layout = dbc.Container(
             storage_type='session'
         ),
         dbc.Row(
-            html.Header(
-                dbc.Row(
-                    dbc.Col(
-                        header,
-                        className='mb-2',
-                        width={'size': 12}
-                    ),
-                    justify='center'
+            [
+                dbc.Col(
+                    sidebar,
+                    xs=3, sm=3, md=2, lg=2, xl=2, xxl=2,
+                    style={
+                        "top": 0,
+                        "left": 0,
+                        "bottom": 0,
+                        "width": "14rem",
+                        "padding": "1rem",
+                        "background-color": "#f8f9fa",
+                    }
                 ),
-            ),
-        ),
-        dbc.Row(
-            page_container,
-            class_name='vh-100 gy-5'
-        ),
-        dbc.Row(
-            html.Footer(
-                dbc.Row(
-                    dbc.Col(
-                        footer,
-                        className='mt-2',
-                        width={'size': 12}
+                dbc.Col(
+                    dbc.Container(
+                        [
+                            dbc.Row(
+                                dbc.Col(
+                                    header,
+                                    className='mb-2',
+                                    width={'size': 12}
+                                ),
+                                justify='center'
+                            ),
+                            dbc.Row(
+                                dbc.Col(
+                                    page_container,
+                                    class_name='gy-5',
+                                    xs=9, sm=9, md=10, lg=10, xl=10, xxl=10
+                                )
+                            ),
+                            dbc.Row(
+                                dbc.Row(
+                                    dbc.Col(
+                                        footer,
+                                        className='mt-2',
+                                        width={'size': 12}
+                                    ),
+                                    justify='center'
+                                ),
+                            )
+                        ],
+                        fluid=True,
+                        class_name='p-0'
                     ),
-                    justify='center'
-                ),
-            )
+                    class_name='p-0'
+                )
+            ],
         )
     ],
     fluid=True,
