e327e72eb6ca90cad8654c4d12aa7d207796c066
Mel Chafart <141696933+mchaf0126@users.noreply.github.com>
2025-01-13 19:19:16 -0800

    feat: finish model comparison tab

--- src/components/model_comp_components.py
+++ src/components/model_comp_components.py
@@ -1,4 +1,4 @@
-from dash import html, dcc, callback, Input, Output, State, ALL
+from dash import html, dcc, callback, Input, Output, State
 import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.express as px
@@ -8,7 +8,6 @@ import src.components.construction_components as cc
 import src.components.replacement_components as rc
 import src.components.eol_components as ec
 from src.utils.load_config import app_config
-from src.components.descriptions import description_map, description_list
 
 config = app_config
 
@@ -92,31 +91,29 @@ model_comp_layout = html.Div(
                     dbc.Col(
                         [
                             model_comp_sidebar
-                        ], xs=2, sm=2, md=2, lg=2, xl=2, xxl=2,
+                        ], xs=3, sm=3, md=3, lg=3, xl=3, xxl=3,
                         class_name=''
                     ),
-                    # dbc.Col(
-                    #     [
-                    #         dbc.Container(
-                    #             [
-                    #                 dbc.Row(
-                    #                     dcc.Graph(id="se_figure"),
-                    #                 )
-                    #             ],
-                    #             class_name='mt-2',
-                    #             fluid=True
-                    #         )
-                    #     ], xs=7, sm=7, md=7, lg=7, xl=7, xxl=7,
-                    #     class_name=''
-                    # ),
-                    # dbc.Col(
-                    #     [
-                    #         html.Div(
-                    #             id='se_description',
-                    #             className='pt-2'
-                    #         )
-                    #     ], xs=3, sm=3, md=3, lg=3, xl=3, xxl=3
-                    # ),
+                    dbc.Col(
+                        [
+                            dbc.Container(
+                                [
+                                    dbc.Row(
+                                        dcc.Graph(id="mc_figure"),
+                                        class_name='mt-2',
+                                    ),
+                                    dbc.Row(
+                                        html.Div(
+                                            id='mc_description',
+                                            className='pt-2'
+                                        )
+                                    ),
+                                ],
+                                fluid=True,
+                                class_name='m-3'
+                            )
+                        ], xs=9, sm=9, md=9, lg=9, xl=9, xxl=9,
+                    ),
                 ],
                 # justify='center',
                 className=''
@@ -128,108 +125,110 @@ model_comp_layout = html.Div(
 )
 
 
-# @callback(
-#     Output('scenario_card', 'children'),
-#     Input('life_cycle_stage_dropdown', 'value')
-# )
-# def update_scenario_card(life_cycle_stage):
-#     if life_cycle_stage == 'Transportation':
-#         return transportation_scenarios
-#     elif life_cycle_stage == 'Construction':
-#         return construction_scenarios
-#     elif life_cycle_stage == 'Replacement':
-#         return replacement_scenarios
-#     elif life_cycle_stage == 'End-of-life':
-#         return eol_scenarios
-#     else:
-#         return "try again!"
+@callback(
+    Output('mc_figure', 'figure'),
+    [
+        Input('impact_dropdown_model_comp', 'value'),
+        Input('scope_dropdown_model_comp', 'value'),
+        Input('transporation_scenario_radio', 'value'),
+        # Input('construction_scenario_radio', 'value'),
+        Input('replacement_scenario_radio', 'value'),
+        Input('eol_scenario_radio', 'value'),
+        State('template_model_name', 'data'),
+        State('template_model_impacts', 'data'),
+        State('prebuilt_scenario_impacts', 'data'),
+    ]
+)
+def update_se_figure(impact: str,
+                     scope: str,
+                     trans_scenario: str,
+                     # constr_scenario: str,
+                     repl_scenario: str,
+                     eol_scenario: str,
+                     template_model_name: dict,
+                     template_model_impacts: dict,
+                     prebuilt_scenario_impacts: dict):
+
+    lcs_map = {
+        '[A4] Transportation': trans_scenario,
+        # '[A5] Construction': constr_scenario,
+        '[B2-B5] Maintenance and Replacement': repl_scenario,
+        '[C2-C4] End of Life': eol_scenario
+    }
+    tm_impacts_df = pd.DataFrame.from_dict(template_model_impacts.get('tm_impacts'))
+    pb_impacts_df = pd.DataFrame.from_dict(
+        prebuilt_scenario_impacts.get('prebuilt_scenario_impacts')
+    )
+    unpacked_tm_name = template_model_name.get('template_model_value')
+    tm_df_to_graph = tm_impacts_df[
+        (tm_impacts_df['Revit model'] == unpacked_tm_name)
+
+    ].copy()
+    tm_df_to_graph.loc[:, 'model_comp'] = 'Default'
+
+    user_selected_df_to_concat = []
+    user_selected_df_to_concat.append(tm_df_to_graph[tm_df_to_graph['Life Cycle Stage'] == '[A1-A3] Product'])
+    user_selected_df_to_concat.append(tm_df_to_graph[tm_df_to_graph['Life Cycle Stage'] == '[D] Module D'])
+    for lcs, selected_scenario in lcs_map.items():
+        if selected_scenario == 'default':
+            temp_df = tm_df_to_graph[tm_df_to_graph['Life Cycle Stage'] == lcs]
+            user_selected_df_to_concat.append(temp_df)
+        else:
+            temp_df = pb_impacts_df[
+                (
+                    (pb_impacts_df['Revit model'] == unpacked_tm_name)
+                    & (pb_impacts_df['Life Cycle Stage'] == lcs)
+                    & (pb_impacts_df['scenario'] == selected_scenario)
+                )
+            ]
+            user_selected_df_to_concat.append(temp_df)
+
+    user_selected_df_to_graph = pd.concat(user_selected_df_to_concat)
+    user_selected_df_to_graph.loc[:, 'model_comp'] = 'User selected custom scenarios'
+
+    combined_df_to_graph = pd.concat([tm_df_to_graph, user_selected_df_to_graph])
+    combined_df_to_graph = combined_df_to_graph.sort_values('model_comp')
+
+    fig = px.histogram(
+        combined_df_to_graph,
+        x='model_comp',
+        y=impact,
+        color=scope,
+        # title=f'GWP Impacts of {unpacked_tm_name}',
+        height=600,
+    ).update_yaxes(
+        title='',
+        tickformat=',.0f',
+    ).update_xaxes(
+        title='',
+    ).update_layout(
+        # showlegend=False
+    )
+    return fig
 
 
-# @callback(
-#     Output('se_figure', 'figure'),
-#     [
-#         Input('life_cycle_stage_dropdown', 'value'),
-#         Input('impact_dropdown', 'value'),
-#         Input('scope_dropdown', 'value'),
-#         Input({'type': 'prebuilt_scenario', 'id': ALL}, 'value'),
-#         State('template_model_name', 'data'),
-#         State('template_model_impacts', 'data'),
-#         State('prebuilt_scenario_impacts', 'data'),
-#     ]
-# )
-# def update_se_figure(life_cycle_stage: str,
-#                      impact: str,
-#                      scope: str,
-#                      checklist: list,
-#                      template_model_name: dict,
-#                      template_model_impacts: dict,
-#                      prebuilt_scenario_impacts: dict):
-
-#     lcs_map = {
-#         'Transportation': '[A4] Transportation',
-#         'Construction': '[A5] Construction',
-#         'Replacement': '[B2-B5] Maintenance and Replacement',
-#         'End-of-life': '[C2-C4] End of Life'
-#     }
-#     tm_impacts_df = pd.DataFrame.from_dict(template_model_impacts.get('tm_impacts'))
-#     pb_impacts_df = pd.DataFrame.from_dict(
-#         prebuilt_scenario_impacts.get('prebuilt_scenario_impacts')
-#     )
-#     unpacked_tm_name = template_model_name.get('template_model_value')
-#     tm_df_to_graph = tm_impacts_df[
-#         (tm_impacts_df['Revit model'] == unpacked_tm_name)
-#         & (tm_impacts_df['Life Cycle Stage'] == lcs_map.get(life_cycle_stage))
-#     ]
-#     pb_df_to_graph = pb_impacts_df[
-#         (pb_impacts_df['Revit model'] == unpacked_tm_name)
-#         & (pb_impacts_df['Life Cycle Stage'] == lcs_map.get(life_cycle_stage))
-#         & (pb_impacts_df['scenario'].isin(sum(checklist, [])))
-#     ]
-
-#     categories = category_orders.get(life_cycle_stage)
-
-#     combined_df_to_graph = pd.concat([tm_df_to_graph, pb_df_to_graph])
-#     combined_df_to_graph['scenario'] = pd.Categorical(
-#         combined_df_to_graph['scenario'],
-#         ordered=True,
-#         categories=categories
-#     )
-#     combined_df_to_graph = combined_df_to_graph.sort_values('scenario')
-
-#     fig = px.histogram(
-#         combined_df_to_graph,
-#         x='scenario',
-#         y=impact,
-#         color=scope,
-#         # title=f'GWP Impacts of {unpacked_tm_name}',
-#         height=600,
-#     ).update_yaxes(
-#         title='',
-#         tickformat=',.0f',
-#     ).update_xaxes(
-#         title='',
-#     ).update_layout(
-#         # showlegend=False
-#     )
-#     return fig
-
-
-# @callback(
-#     Output('se_description', 'children'),
-#     [
-#         Input({'type': 'prebuilt_scenario', 'id': ALL}, 'value'),
-#     ]
-# )
-# def update_description(checklist):
-#     flattened_checklist = sum(checklist, [])
-#     sorted_list = sorted(flattened_checklist, key=description_list.index)
-#     title = [
-#         dcc.Markdown(
-#             '''
-#             ### Descriptions
-#             See below for a description of the different scenarios that have been selected.
-#             ''',
-#             className='fw-light'
-#         )
-#     ]
-#     return title + [description_map.get(value) for value in sorted_list]
+@callback(
+    Output('mc_description', 'children'),
+    [
+        Input('transporation_scenario_radio', 'value'),
+        Input('construction_scenario_radio', 'value'),
+        Input('replacement_scenario_radio', 'value'),
+        Input('eol_scenario_radio', 'value'),
+    ]
+)
+def update_description(trans_scenario: str,
+                       constr_scenario: str,
+                       repl_scenario: str,
+                       eol_scenario: str,):
+    text = dcc.Markdown(
+        f'''
+        ### User selected custom scenarios
+        The following scenarios have been selected:
+        - Transportation: {trans_scenario}
+        - Construction: {constr_scenario}
+        - Replacement: {repl_scenario}
+        - End-of-life: {eol_scenario}
+        ''',
+        className='fw-light'
+    )
+    return text
